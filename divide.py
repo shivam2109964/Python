de34955e440d2f8230fd5ed2af0b38815a6cfd6b
Shivam Gupta <sg2109964@gmail.com>
2025-06-25 16:11:49 +0530

    Done

---
+++ divide.py
@@ -0,0 +1,6 @@
+value: int = int(input("Enter the first number: "))
+value_one: int = int(input("Enter the second number: "))
+
+result: int = value // value_one
+
+print(f" result is {result}")
---
+++ sum.py
@@ -0,0 +1,7 @@
+value: int = int(input("Enter the first number: "))
+value_one: int = int(input("Enter the second number: "))
+
+sum: int = value + value_one
+
+print(f"Result is {sum}")
+
