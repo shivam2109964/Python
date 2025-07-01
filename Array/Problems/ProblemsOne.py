9cb19cf7c4c2e49c4b6ce23e58a63e6fbe1465f8
Shivam Gupta <sg2109964@gmail.com>
2025-07-01 17:31:48 +0530

    Problem One Done

--- Array/Problems/ProblemsOne.py
+++ Array/Problems/ProblemsOne.py
@@ -1,3 +1,4 @@
+# Given an array arr. The task is to find the largest element in the given array. 
 def largest(array, length):
     max = array[0]
     for i in range(1, length):
@@ -5,8 +6,21 @@ def largest(array, length):
             max = array[i]
     return max
 
+def findMax(array, index):
+    if(index == len(array) - 1):
+        return array[index]
+    recMax = findMax(array, index + 1)
+    return max(recMax, array[index])
+
+def largestRec(array):
+    return findMax(array, 0)
+
+
+
 length = int(input("Enter the length of an array: "))
 array = [int(input(f"Enter the {i + 1} element: ")) for i in range(length)]
 largestElement = largest(array, length)
 print(f"Largest element is: {largestElement}")
+result = largestRec(array)
+print(f"Largest element by Recursive {result}")
     
